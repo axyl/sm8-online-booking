aac14a1166f93b436c88cc6a21f4ea8918d0df55
Craig <craig@finnewsnetwork.com.au>
2024-09-25 14:08:19 +1000

    setting up secrets

--- api/index.js
+++ api/index.js
@@ -3,11 +3,11 @@ module.exports.handler = async (event) => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: "Go Serverless v3.0! Your function executed successfully!",
+        message: `Go Serverless v3.0! Your function executed successfully! Env Variable is: ${process.env.test}`,
         input: event,
       },
       null,
-      2
+      2,
     ),
   };
 };
